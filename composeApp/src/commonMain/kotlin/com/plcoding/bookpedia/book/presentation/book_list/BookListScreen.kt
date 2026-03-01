0079a4ceee650b289ec0498fb105ebd480e849b5
LCDII <piskunov.leon@gmail.com>
2026-03-01 23:06:55 +0300

    added book model

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -0,0 +1,20 @@
+package com.plcoding.bookpedia.book.presentation.book_list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BookListScreenRoot(
+    modifier: Modifier = Modifier
+
+) {
+
+}
+
+
+@Composable
+fun BookListScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
