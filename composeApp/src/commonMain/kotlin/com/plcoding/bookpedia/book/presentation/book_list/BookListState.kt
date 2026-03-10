c54986eece18382452934916f5ed96c49f7ea860
LCDII <piskunov.leon@gmail.com>
2026-03-10 20:07:15 +0300

    fixed loading in andriod and added Koin

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -3,21 +3,6 @@ package com.plcoding.bookpedia.book.presentation.book_list
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.UiText
 
-val books = (1..100).map {
-    Book(
-        id = it.toString(),
-        title = "Book $it",
-        imageURL = "https://notworking.com",
-        authors = listOf("LevPiskunov"),
-        description = "Description $it",
-        languages = emptyList(),
-        publishedYear = null,
-        averageRating = 4.5,
-        ratingCount = 5,
-        numberOfPages = 300,
-        numberOfEditions = 2
-    )
-}
 data class BookListState(//Just all variables we use while BookList layer is working
     val searchQuery: String = "Kotlin",
     val searchResult: List<Book> = emptyList(),
